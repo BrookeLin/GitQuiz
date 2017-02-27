de8e080316c05ded8e30022b0eb29bfb12b5d96e
glenn <glenncorey23@gmail.com>
2017-02-27 16:35:00 -0500

    Glenn First Commit

--- GitQuiz/App.java
+++ GitQuiz/App.java
@@ -4,6 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//HEY GLENN, YOU LOSER
+		// nice space after the slash marks, nice formatting loser
 	}
 
 }
